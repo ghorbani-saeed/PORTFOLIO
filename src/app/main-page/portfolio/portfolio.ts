e0de40c928c142a0dc73ded816789937ee1eae93
ghorbani-saeed <ghorbani.saeed@web.de>
2026-06-21 13:41:28 +0200

    updated portfolio to use new project variables

---
+++ src/app/main-page/portfolio/portfolio.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UserDatabankService } from '../../shared/components/userDatabankService/user-databank-service';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [TranslatePipe],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.scss',
+})
+
+export class Portfolio {
+  userDBS = inject(UserDatabankService);
+
+  getCounterLabel(index: number): string {
+    return `0${index + 1}/0${this.userDBS.showcaseProjects.length}`;
+  }
+}
