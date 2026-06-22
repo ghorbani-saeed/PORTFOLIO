b513389cbbf3e07e9718dc0483b6d3dd2255e19a
ghorbani-saeed <ghorbani.saeed@web.de>
2026-06-22 08:14:43 +0200

    style: apply global bounce animation to legal notice back button

---
+++ src/app/landing-page/legal-notice/legal-notice.ts
@@ -0,0 +1,14 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UserDatabankService } from '../../shared/components/userDatabankService/user-databank-service';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [RouterLink, TranslatePipe],
+  templateUrl: './legal-notice.html',
+  styleUrl: './legal-notice.scss',
+})
+export class LegalNotice {
+  userDBS = inject(UserDatabankService);
+}
