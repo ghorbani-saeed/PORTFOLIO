1aef843f7c3d61be11c0fd649a3936fc7afcb7ac
ghorbani-saeed <ghorbani.saeed@web.de>
2026-07-07 16:40:49 +0200

    change email address to a more professional address: info@saeed-ghorbani.de

--- public/sendMail.php
+++ public/sendMail.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=utf-8");
 
-$siteEmail = "saeed.ghorbani@saeed-ghorbani.de";
+$siteEmail = "info@saeed-ghorbani.de";
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'OPTIONS':
